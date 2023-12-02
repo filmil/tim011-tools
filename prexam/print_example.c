4c90142db7d79fa9c5480129cf616bdebb5dcd46
Filip Filmar <filmil@gmail.com>
2023-12-02 03:28:31 +0000

    fix: bazel run //prexam:emu2 now works
    I tried some simple tricks to try and have it
    run faster. It didn't really work.

--- prexam/print_example.c
+++ prexam/print_example.c
@@ -1,24 +1,23 @@
-extern char* timfont;
-extern char zxchars[];
 
-void prstr(const char* s);
-void prsetinv(int v);
+// From //lib/tim:tim_print_lib
+void inline prstr(const char* s) __sdcccall(1) __naked;
+void inline prsetinv(char v) __sdcccall(1) __naked;
+
+extern char chrinv;
 
 int main(void) {
-    // Initialize tim's font.
-    timfont = zxchars;
-
     const char* str1 = "Trla Baba Lan Da Joj Prodje Dan! ";
     const char* str2 = "www.onceuponabyte.org ";
     const char* str3 = "WWW.8BITCHIP.INFO ";
+
     for(int i = 0; i < 28; i++) {
         prstr(str1);
         prstr(str2);
         prstr(str3);
-        if (i&1)
-            prsetinv(0x8FF);
-        else
-            prsetinv(0x800);
+        prsetinv((i & 1) ? 0xff : 0x00);
+        // Doesn't seem to be any faster.  Standard tricks like loop unroll
+                // also don't seemto do anything special.
+        //chrinv = (i & 1) ? 0xff : 0x00;
     }
     return 0;
 }
